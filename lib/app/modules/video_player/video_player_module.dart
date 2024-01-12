59e291c21be5eb675b8be0914c21a6ca642abf3c
Joao Cabral <joaocabral1232@gmail.com>
2024-01-12 18:51:05 -0400

    feat: file created to module

---
+++ lib/app/modules/video_player/video_player_module.dart
@@ -0,0 +1,16 @@
+import 'package:librascode/app/modules/video_player/video_player_page.dart';
+import 'package:provider/provider.dart';
+
+import '../../core/modules/libras_code_list_module.dart';
+import 'video_player_controller.dart';
+
+class VideoPlayerModule extends LibrasCodeListModule {
+  VideoPlayerModule()
+      : super(bindings: [
+          ChangeNotifierProvider(
+            create: (_) => VideoPlayerController(),
+          ),
+        ], routers: {
+          '/history': (context) => const VideoPlayerPage()
+        });
+}
---
+++ lib/app/modules/video_player/video_player_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class VideoPlayerPage extends StatefulWidget {
+  const VideoPlayerPage({super.key});
+
+  @override
+  State<VideoPlayerPage> createState() => _VideoPlayerPageState();
+}
+
+class _VideoPlayerPageState extends State<VideoPlayerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
