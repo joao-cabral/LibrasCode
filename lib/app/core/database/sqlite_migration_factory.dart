8cddc3fdfd49a778082e71a545c8d1afa6e7da6c
João Cabral <joaocabral1232@gmail.com>
2024-05-15 00:32:53 -0400

    chore: path changes

--- lib/app/core/database/sqlite_migration_factory.dart
+++
@@ -1,7 +0,0 @@
-import 'package:librascode/app/core/database/migrations.dart/migration.dart';
-import 'package:librascode/app/core/database/migrations.dart/migration_v1.dart';
-
-class SqliteMigrationFactory {
-  List<Migration> getCreateMigration() => [MigrationV1()];
-  List<Migration> getUpdateMigration(int version) => [];
-}
