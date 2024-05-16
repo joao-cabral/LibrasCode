84fad7409fda153678b32098ba646aa28a3bed52
João Cabral <joaocabral1232@gmail.com>
2024-05-16 01:26:41 -0400

    feat: modules home, qrcode and historic created

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:librascode/app/modules/core/core_module.dart';
+import 'package:librascode/app/modules/qrcode/qrcode_module.dart';
 
-import 'modules/home/home_page.dart';
-import 'modules/video_player/videio_player_module.dart';
+import 'modules/historic/historic_module.dart';
+import 'modules/home/home_module.dart';
+import 'modules/video_player/video_player_module.dart';
 
 class AppModule extends Module {
   @override
@@ -15,7 +17,9 @@ class AppModule extends Module {
 
   @override
   void routes(r) {
-    r.child('/', child: (context) => const HomePage());
+    r.module('/', module: HomeModule());
+    r.module('/qr-code', module: QRCodeModule());
     r.module('/video-player', module: VideioPlayerModule());
+    r.module('/history', module: HistoricModule());
   }
 }
