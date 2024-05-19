88801b1cebe8e78139eff255da69475282da8974
João Cabral <joaocabral1232@gmail.com>
2024-05-18 20:17:23 -0400

    feat: page changed and components created

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,9 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../models/historic_model.dart';
+import '../../services/historic/historic_service.dart';
+
 class HomeController {
-  String? _result;
+  final HistoricService _historicService;
 
-  String? get result => _result;
+  HomeController({required HistoricService historicService})
+      : _historicService = historicService;
 
-  set result(String? newValue) {
-    _result = newValue;
+  ValueNotifier<List<HistoricModel>> historic = ValueNotifier([]);
+  ValueNotifier<bool> loading = ValueNotifier(false);
+
+  Future<void> getAll() async {
+    try {
+      loading.value = true;
+      historic.value = await _historicService.getAll();
+      loading.value = false;
+    } catch (error) {
+      print(error);
+    }
   }
 }
