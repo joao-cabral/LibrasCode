69bf0ea74f0f481f6a0df7ece85a710313e310b1
João Cabral <joaocabral1232@gmail.com>
2024-05-23 01:59:35 -0400

    feat: video player with historic

--- lib/app/modules/video_player/video_player_controller.dart
+++ lib/app/modules/video_player/video_player_controller.dart
@@ -1,13 +1,18 @@
+import 'package:flutter/material.dart';
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 import '../../models/historic_model.dart';
 import '../../services/historic/historic_service.dart';
 
-class VideoPlayerController {
+class VideoPlayerController extends ChangeNotifier {
   final HistoricService _historicService;
 
   VideoPlayerController({required HistoricService historicService})
       : _historicService = historicService;
+
+  List<HistoricModel> historic = [];
+  bool loading = false;
+  String videoTitle = '';
 
   YoutubePlayerController configYoutubePLayer(String videoId) {
     return YoutubePlayerController.fromVideoId(
@@ -31,5 +36,21 @@ class VideoPlayerController {
         watchDate: DateTime.now(),
       ),
     );
+  }
+
+  Future<void> getAll() async {
+    loading = true;
+    try {
+      historic = await _historicService.getAll();
+      loading = false;
+    } catch (error) {
+      print(error);
+    }
+    notifyListeners();
+  }
+
+  void getTitle(String title) {
+    videoTitle = title;
+    notifyListeners();
   }
 }
--- lib/app/modules/video_player/video_player_module.dart
+++ lib/app/modules/video_player/video_player_module.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:librascode/app/modules/video_player/video_player_controller.dart';
+import 'package:provider/provider.dart';
 
 import '../core/core_module.dart';
 import 'video_player_page.dart';
@@ -19,9 +20,12 @@ class VideioPlayerModule extends Module {
   void routes(r) {
     r.child(
       '/',
-      child: (_) => VideoPlayerPage(
-        videoId: r.args.queryParams['videoId'],
-        controller: Modular.get<VideoPlayerController>(),
+      child: (_) => ChangeNotifierProvider(
+        create: (context) => Modular.get<VideoPlayerController>(),
+        child: VideoPlayerPage(
+          videoId: r.args.queryParams['videoId'],
+          controller: Modular.get<VideoPlayerController>(),
+        ),
       ),
     );
   }
